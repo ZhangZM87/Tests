1c206b00bd312d9fbf2c301ca714c085634bfa86
张志民 <2811733392@qq.com>
2025-08-22 14:50:36 +0800

    获取本地所有打印机

--- PrinterStatusLister/Program.cs
+++ PrinterStatusLister/Program.cs
@@ -1,5 +1,6 @@
 ﻿using PrinterService.Listener;
 using System;
+using System.Printing;
 
 namespace PrinterStatusLister
 {
@@ -8,7 +9,22 @@ namespace PrinterStatusLister
         static void Main(string[] args)
         {
             new NewPrintJobRemoveListener().StartListener();
-
+            var printServer = new LocalPrintServer();
+            // 获取所有打印队列
+            using (var printQueues = printServer.GetPrintQueues(
+                //new[] {
+                //    EnumeratedPrintQueueTypes.Local,
+                //    EnumeratedPrintQueueTypes.Connections,
+                //    EnumeratedPrintQueueTypes.Shared,
+                //    EnumeratedPrintQueueTypes.Fax
+                //}
+        ))
+            {
+                foreach (var printQueue in printQueues)
+                {
+                    Console.WriteLine($"检测打印机名称 {printQueue.Name}");
+                }
+            };
             Console.ReadKey();
         }
     }
